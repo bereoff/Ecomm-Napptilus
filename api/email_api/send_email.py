b8fcae7b3af0fbab8231bf839cfa1aef717b7e72
Bruno Bereoff <bbereoff@gmail.com>
2023-12-12 00:16:21 +0100

    updated: refactoring send email functions

--- api/email_api/send_email.py
+++ api/email_api/send_email.py
@@ -1,8 +1,64 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
+# ↓↓↓↓↓ prompt functions
 
-def send_mail(customer_data, api_key):
+
+class PromptEmail():
+    def __init__(self, contact_data=None):
+        self.contact_data = contact_data
+
+    def send_prompt_email_purchase(self):
+        """Mock email sending by printing to a file"""
+
+        import random
+        import string
+        length = 12
+        chars = string.digits
+
+        customer_name = self.contact_data.get("name").title(
+        ) + " " + self.contact_data.get("surname").title()
+
+        order_number = ''.join(random.choice(chars) for i in range(length))
+
+        PURCHASE_MAIL = f"""
+        From: Napptilus Ecomm <no-reply@napptilus-ecomm.com>
+        To: {self.contact_data.get("email")}
+        Subject: f"Ecomm-Napptilus Order {order_number} 🎁🎁"
+
+        Hello,
+        {customer_name}
+        We are preparing your purchases with great care 🥰.
+        To track the status of your order {order_number}, please visit our website "https://napptilus.com/".
+        Keep counting on us. ❤️❤️
+        Ecomm-Napptilus Team  """
+
+        return PURCHASE_MAIL
+
+    def send_prompt_email_report(email):
+        from datetime import datetime
+
+        import pytz
+
+        """Mock email sending by printing to a file"""
+
+        REPORT_MAIL = f"""
+        From: Napptilus Ecomm <tech@napptilus-ecomm.com>
+        To: {email}
+        Subject: f"Inventory accuracy report submission - {datetime.now(pytz.utc)}"
+
+        Attached is the link to the inventory report of the products "https://napptilus.com/".
+        Please do not unsubscribe from receiving this email
+        If you have any questions or issues, please contact your supervisor
+        """
+
+        return REPORT_MAIL
+
+
+# ↓↓↓↓↓ queue sendgrid backend functions
+
+
+def purchase_email_sending(customer_data, SENDGRID_API_KEY):
     import random
     import string
     length = 12
@@ -37,7 +93,40 @@ def send_mail(customer_data, api_key):
     )
 
     try:
-        response = SendGridAPIClient(api_key).send(message)
+        response = SendGridAPIClient(SENDGRID_API_KEY).send(message)
         return response.status_code
+    except Exception as e:
+        print(e)
+
+
+def report_email_sending(email, SENDGRID_API_KEY):
+    from datetime import datetime
+
+    import pytz
+
+    html_message = """
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Order Status</title>
+                </head>
+                <body>
+                    <p>Attached is the link to the inventory report of the products <a href="https://napptilus.com/">our website</a>.</p>
+                    <p>Please do not unsubscribe from receiving this email.</p>
+                    <p>If you have any questions or issues, please contact your supervisor.</p>
+                </body>
+                </html>
+                """
+    message = Mail(
+        from_email=('bbereoff@gmail.com', 'Ecomm-Napptilus'),
+        to_emails=[email],
+        html_content=html_message,
+        subject=f"Inventory accuracy report submission - {datetime.now(pytz.utc)}",
+    )
+
+    try:
+        response = SendGridAPIClient(SENDGRID_API_KEY).send(message)
+        return response
     except Exception as e:
         print(e)
