c595a643692d2db7709c1e0f8e42430fcdfff666
Bruno Bereoff <bbereoff@gmail.com>
2023-12-12 00:22:30 +0100

    added: entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+## Fixture
+# Transfer images to media folder
+
+# # Database migrations
+echo " "
+echo "###############################################"
+# echo "Applying database migrations"
+# python manage.py migrate
+
+# # Test Pipeline
+# echo " "
+# echo "Apply test pipeline"
+# python -m pytest
+# echo " "
+
+## Fixture
+# Create specific folders
+mkdir -p ./media/product/T-Shirt
+mkdir -p ./media/product/Cap
+
+# Transfer images to media folder
+find ./fixture-images -iname '*t-shirt*' -exec cp {} ./media/product/T-Shirt/ \;
+find ./fixture-images -iname '*cap*' -exec cp {} ./media/product/Cap/ \;
+
+$@
